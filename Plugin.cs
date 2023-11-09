fe6b271ceb2edba85f0a5ec59c8099b280f21996
GBN <58118380+GraczBezNicku@users.noreply.github.com>
2023-11-09 17:10:11 +0100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,18 @@
+﻿using PluginAPI.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpsCommand
+{
+    public class Plugin
+    {
+        [PluginEntryPoint("TPS Command", "1.0.0", "Shows the current TPS", "GBN")]
+        public void EntryPoint()
+        {
+            // Nothing ??
+        }
+    }
+}
---
+++ TpsCommand.cs
@@ -0,0 +1,28 @@
+﻿using CommandSystem;
+using PluginAPI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ICommand = CommandSystem.ICommand;
+
+namespace TpsCommand
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class RegenerateMap : ICommand
+    {
+        public string Command => "Tps";
+
+        public string[] Aliases { get; } = { "tps" };
+
+        public string Description => "";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = $"Current TPS: {Server.TPS}";
+            return true;
+        }
+    }
+}
